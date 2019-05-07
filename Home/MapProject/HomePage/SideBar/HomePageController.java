fad312d06ee990f4484626c7ff750bfe64cc302a
duylebkHCM <49871303+duylebkHCM@users.noreply.github.com>
2019-05-08 00:58:44 +0700

    Add function for 3 navigation buttons and change attribute of borderpane

--- Home/MapProject/HomePage/SideBar/HomePageController.java
+++ Home/MapProject/HomePage/SideBar/HomePageController.java
@@ -1,8 +1,6 @@
 package Home.MapProject.HomePage.SideBar;
 
 
-
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
@@ -12,13 +10,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 
@@ -38,11 +37,28 @@ public class HomePageController implements Initializable {
     
     @FXML
     public JFXButton btnLogout;
+    
+    @FXML
+	public JFXButton btnmap;
+
+    @FXML
+    public JFXButton btnedit;
+    
+    @FXML
+    public JFXButton btndash;
+
+    @FXML
+    private BorderPane bdpane;
+    
+    @FXML
+    
+    public GridPane grid;
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("SidePanel.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Home/MapProject/HomePage/SideBar/SidePanel.fxml"));
             VBox box = loader.load();
             SidePanelController controller = loader.getController();
             controller.setCallback(this);
@@ -50,13 +66,19 @@ public class HomePageController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(HomePageController.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
+        bdpane.setVisible(false);
         HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
         transition.setRate(-1);
         hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
             transition.setRate(transition.getRate() * -1);
             transition.play();
-
+            if(bdpane.isVisible()) {
+            	   bdpane.setVisible(false);
+            }
+            else {
+            	 bdpane.setVisible(true);
+            }
             if (drawer.isOpened()) {
                 drawer.close();
             } else {
@@ -66,8 +88,24 @@ public class HomePageController implements Initializable {
         
         btnLogout.setVisible(true);
         btnLogout.setOnAction(e->{
-        	LoadUi("../../Select/Select.fxml", root);
+        	LoadUi("/Home/MapProject/Select/Select.fxml", root);
         });
+        
+        btnmap.setVisible(true);
+        btnmap.setOnAction(e->{
+        	LoadUi("/Home/MapProject/HomePage/Mappage/Map.fxml", anchorPane);
+        });
+        
+        btnedit.setVisible(true);
+        btnedit.setOnAction(e->{
+        	LoadUi("/Home/MapProject/HomePage/EditPage/Edit.fxml", anchorPane);
+        });
+        
+        btndash.setVisible(true);
+        btndash.setOnAction(e->{
+        	LoadUi("/Home/MapProject/HomePage/DashBoardPage/DashBoard.fxml", anchorPane);
+        });
+       
     }
     public void LoadUi(String UI, AnchorPane ref) {
     	Parent root1= null;
@@ -82,6 +120,10 @@ public class HomePageController implements Initializable {
     	ref.setLeftAnchor(root1, 0.0);
     	ref.setRightAnchor(root1, 0.0);
     	btnLogout.setVisible(false);
+    	btnmap.setVisible(false);
+    	btnedit.setVisible(false);
+    	btndash.setVisible(false);
+    	grid.setDisable(true);
     }
  
 }
